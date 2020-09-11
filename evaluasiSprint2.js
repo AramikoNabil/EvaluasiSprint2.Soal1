4560914ccfe482e686b7a49585affc51e658742e
sleepy81k <nabilnico@gmail.com>
2020-09-12 06:12:43 +0700

    first

---
+++ evaluasiSprint2.js
@@ -0,0 +1,10 @@
+var nilaiSantri = [ 50, 90, 65, 67, 89, 80, 50, 97, 84 ]
+var total = 0
+var banyakNilai = nilaiSantri.length
+
+for(i = 0;  i < banyakNilai; i ++){
+    console.log (i)
+    total += banyakNilai
+}
+
+document.write (total / banyakNilai)
